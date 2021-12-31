e32b7584eb0a7f9d67674abc0147c5673fee1520
Vicente Aranda <vicente.aranda@protonmail.com>
2021-12-30 22:10:07 -0300

    Added Form Functionality

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { useTracker } from 'meteor/react-meteor-data';
 import { DataTable } from './DataTable';
 import { PatientForm } from './PatientForm';
 import { PatientsCollection } from '../database/PatientsCollection';
-
+import { Fields } from './PatientForm';
 
 export const App = () => {
 
+  // Get patients from db
   const patients = useTracker(() => PatientsCollection.find({}).fetch());
+
+  const { reset, formState: { isSubmitSuccessful } } = useForm<Fields>();
+
+  const onSubmit:SubmitHandler<Fields> = data => {
+    PatientsCollection.insert(data);
+  };
+
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset({});
+    } 
+  }, [ isSubmitSuccessful, SubmitEvent ]);
   
   return (
     <main>
       <section id="form-section">
-        <PatientForm />
+        <PatientForm onSubmit={ onSubmit }/>
       </section>
       <section id="data-section">
         <DataTable patients={ patients } />
--- imports/ui/PatientForm.tsx
+++ imports/ui/PatientForm.tsx
@@ -1,42 +1,64 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Patient } from '../database/PatientsCollection';
 
-export const PatientForm = () => {
+
+export type Fields = Patient;
+
+type PatientFormProps = {
+    onSubmit: (data: any) => void
+};
+
+
+export const PatientForm = (props: PatientFormProps) => {
+
+    // Register new patients
+    const { register, handleSubmit, formState: {errors} } = useForm<Fields>();
+
     return (
-        <form>
+        <form onSubmit={ handleSubmit(props.onSubmit) }>
             <div className="form-group">
-                <label htmlFor="nombres">Nombres</label>
-                <input type="text" id="nombres" placeholder="Nombres"/>
+                <label htmlFor="name">Nombres</label>
+                <input type="text" { ...register("name", { required: true }) } id="name" placeholder="Nombres"/>
+                { errors.name && <span>Este campo es obligatorio</span> }
             </div>
             <div className="form-group">
-                <label htmlFor="ap-pat">Apellido Paterno</label>
-                <input type="text" id="ap-pat" placeholder="Apellido Paterno"/>
+                <label htmlFor="fatherSurname">Apellido Paterno</label>
+                <input type="text" { ...register("fatherSurname", { required: true }) } id="fatherSurname" placeholder="Apellido Paterno"/>
+                { errors.fatherSurname && <span>Este campo es obligatorio</span> }
             </div>
             <div className="form-group">
-                <label htmlFor="ap-mat">Apellido Materno</label>
-                <input type="text" id="ap-mat" placeholder="Apellido Materno"/>
+                <label htmlFor="motherSurname">Apellido Materno</label>
+                <input type="text" { ...register("motherSurname", { required: true }) } id="motherSurname" placeholder="Apellido Materno"/>
+                { errors.motherSurname && <span>Este campo es obligatorio</span> }
             </div>
             <div className="form-group">
                 <label htmlFor="rut">RUT</label>
-                <input type="text" id="rut" placeholder="RUT"/>
+                <input type="text" { ...register("rut", { required: true }) } id="rut" placeholder="RUT"/>
+                { errors.rut && <span>Este campo es obligatorio</span> }
             </div>
             <div className="form-group">
-                <label htmlFor="region">Región</label>
-                <select name="region" id="region">
-                    <option value="">1</option>
-                    <option value="">2</option>
-                    <option value="">3</option>
-                    <option value="">4</option>
+                <label htmlFor="state">Región</label>
+                <select { ...register("state") } id="state">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
                 </select>
+                { errors.state && <span>Este campo es obligatorio</span> }
             </div>
             <div className="form-group">
-                <label htmlFor="comuna">Comuna</label>
-                <select name="comuna" id="comuna">
-                    <option value="">1</option>
-                    <option value="">2</option>
-                    <option value="">3</option>
-                    <option value="">4</option>
+                <label htmlFor="city">Comuna</label>
+                <select { ...register("city") } id="city">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
                 </select>
+                { errors.city && <span>Este campo es obligatorio</span> }
             </div>
+
+            <button type="submit">Crear Registro</button>
         </form>
     );
 };
